1303273529a928202921ad936af11c5fa9a5d815
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2023-11-18 21:45:15 +0100

    details page

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { ProfitPipe } from './pipes/profit.pipe';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+import { DetailsMoviesComponent } from './components/details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './components/details-reviews/details-reviews.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { HomeComponent } from './components/home/home.component';
     ProfitPipe,
     FooterComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    DetailsComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent
   ],
   imports: [
     BrowserModule
