44254226d4a6944b52e02d718be3d25c60c69db9
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2023-11-18 18:10:20 +0100

    navbar and footer

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProfitPipe } from './pipes/profit.pipe';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfitPipe
+    ProfitPipe,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
