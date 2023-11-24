96c34d3a572d6d28b91b0af9c7ae06e8b681d855
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2023-11-24 23:25:41 +0100

    working on details to be dynamic

--- movies-app/src/app/components/details/details.component.ts
+++ movies-app/src/app/components/details/details.component.ts
@@ -14,5 +14,9 @@ constructor(private _activatedRoute: ActivatedRoute) {
     this.movieId = data['id']
     console.log('id='+ this.movieId);
   })
-}
+}//e kam marr id prej rutit activ edhe e kom ru te movieId
+  
+  stars = ['Star 1']
+  directories = ['Director 1', 'Director 2','Directory']
+  genres = ['Comedy', 'Romance','Thriller','Drama']
 }
